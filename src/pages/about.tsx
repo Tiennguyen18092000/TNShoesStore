fa5cafd1f55cbd829e92154e473c72fd77ca811e
TienNguyen <tiennguyen18092000@gmail.com>
2023-10-30 10:28:07 +0700

    v21

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,16 +10,16 @@ export default function About(){
             <meta name="keywords" content="Shoes"/>
         </Head>
         <div className={styles.contain}>
-            <h1 className={styles.name}>INTRODUCE</h1>
+            <h1 className={styles.name}>Introduce</h1>
             <br/>
             <p className={styles.name}>TNshoesStore specializes in providing high-end branded footwear products of famous brands in the world</p> 
             <p className={styles.name1}>Here you can easily search for high-end footwear easily and quickly. We constantly update the hottest and latest models of major brands, so you can shop happily and quickly.</p>
             <br/>
-            <h1 className={styles.name}>TARGET</h1>
+            <h1 className={styles.name}>Target</h1>
             <p className={styles.name1}>The motto is to bring high-class footwear products to all types of customers, from those with little interest to brand enthusiasts.</p>
             <br/>
             <br/>
-            <h1 className={styles.name}>GENUINE PREMIUM BRAND SHOES, DIFFERENT</h1>
+            <h1 className={styles.name}>Genuine Premium Brand Shoes, Different</h1>
             <p className={styles.name1}>Here you will find major and famous footwear brands in the world. In particular, always update and bring back new products to keep up with international trends.</p>
         </div>
         </>
